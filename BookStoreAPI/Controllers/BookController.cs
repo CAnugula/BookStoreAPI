d09d9ca31419aeb4170f44c9a16c0d2bd6781b2d
Chandu <foo@bar.com>
2021-05-05 20:18:14 +1000

    First commit

---
+++ BookStoreAPI/Controllers/BookController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using RestSharp;
+
+namespace BookStoreAPI.Controllers
+{
+    public class BookController : ApiController
+    {
+        private const string BOOK_URL = "https://simple-books-api.glitch.me";
+
+        [HttpGet]
+        public IHttpActionResult GetStatus() {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAllBooks() 
+        {
+            var client = new RestClient(BOOK_URL);
+            var request = new RestRequest("/books");
+
+            var response = await client.ExecuteAsync(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                return Ok(response.Content);
+            }
+            else
+            {
+                // Log the error
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult postOrder(int bookId, string customerName) {
+            
+            return Ok();
+        }
+    }
+}
